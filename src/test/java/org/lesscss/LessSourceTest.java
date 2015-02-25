b45fbd2f81c16a6d3c491df75acebba93f5d39c4
Gr1ver <gr1ver@mail.ru>
2015-02-25 11:22:24 +0300

    fix for workspaces which path contains whitespaces

--- src/test/java/org/lesscss/LessSourceTest.java
+++ src/test/java/org/lesscss/LessSourceTest.java
@@ -14,33 +14,35 @@
  */
 package org.lesscss;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.AdditionalMatchers;
-import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
-import static org.powermock.api.mockito.PowerMockito.*;
 
 @PrepareForTest({FileUtils.class, IOUtils.class, LessSource.class, FileResource.class})
 @RunWith(PowerMockRunner.class)
@@ -55,11 +57,11 @@ public class LessSourceTest {
     @Mock private LessSource import1;
     @Mock private LessSource import2;
     @Mock private LessSource import3;
-    
+
     private Map<String, LessSource> imports;
 
     private long lastModified = 1l;
-        
+
     @Before
     public void setUp() throws Exception {
         imports = new LinkedHashMap<String, LessSource>();
@@ -67,24 +69,24 @@ public class LessSourceTest {
         imports.put("import2", import2);
         imports.put("import3", import3);
 
-		URL sourceUrl = getClass().getResource("/compatibility/a_source.less");
-        sourceFile = new File(sourceUrl.getFile());
+        URL sourceUrl = getClass().getResource("/compatibility/a_source.less");
+        sourceFile = new File(sourceUrl.toURI().getSchemeSpecificPart());
     }
-    
+
     @Test
     public void testNewLessSourceWithoutImports() throws Exception {
-        
+
         FileResource fileResource = new FileResource(sourceFile);
 
         lessSource = new LessSource(fileResource);
-        
+
         assertEquals(sourceFile.getAbsolutePath(), lessSource.getAbsolutePath());
         assertEquals("content", lessSource.getContent());
         assertEquals("content", lessSource.getNormalizedContent());
         assertEquals(sourceFile.lastModified(), lessSource.getLastModified());
         assertEquals(sourceFile.lastModified(), lessSource.getLastModifiedIncludingImports());
         assertEquals(0, lessSource.getImports().size());
-        
+
         verifyStatic();
         FileUtils.readFileToString(sourceFile);
     }
@@ -93,38 +95,38 @@ public class LessSourceTest {
     public void testNewLessSourceFileNull() throws Exception {
         lessSource = new LessSource((Resource)null);
     }
-    
+
     @Test(expected = IOException.class)
     public void testNewLessSourceFileNotFound() throws Exception {
         when(file.exists()).thenReturn(false);
         lessSource = new LessSource(new FileResource(file));
     }
-    
+
     @Test
     public void testLastModifiedIncludingImportsWhenNoImportModifiedLater() throws Exception {
         mockFile(true,"content","absolutePath");
-        
+
         when(import1.getLastModifiedIncludingImports()).thenReturn(0l);
         when(import2.getLastModifiedIncludingImports()).thenReturn(0l);
         when(import3.getLastModifiedIncludingImports()).thenReturn(0l);
-        
+
         lessSource = new LessSource(new FileResource(file));
         FieldUtils.writeField(lessSource, "imports", imports, true);
-        
+
         assertEquals(1l, lessSource.getLastModifiedIncludingImports());
     }
-    
+
     @Test
     public void testLastModifiedIncludingImportsWhenImportModifiedLater() throws Exception {
         mockFile(true,"content","absolutePath");
-        
+
         when(import1.getLastModifiedIncludingImports()).thenReturn(0l);
         when(import2.getLastModifiedIncludingImports()).thenReturn(2l);
         when(import3.getLastModifiedIncludingImports()).thenReturn(0l);
-        
+
         lessSource = new LessSource(new FileResource(file));
         FieldUtils.writeField(lessSource, "imports", imports, true);
-        
+
         assertEquals(2l, lessSource.getLastModifiedIncludingImports());
     }
 
@@ -140,9 +142,9 @@ public class LessSourceTest {
         assertThat(content, not(containsString("↓")));
     }
 
-    private String readLessSourceWithEncoding(String encoding) throws IOException, IllegalAccessException {
+    private String readLessSourceWithEncoding(String encoding) throws IOException, IllegalAccessException, URISyntaxException {
         URL sourceUrl = getClass().getResource("/compatibility/utf8-content.less");
-        File sourceFile = new File(sourceUrl.getFile());
+        File sourceFile = new File(sourceUrl.toURI().getSchemeSpecificPart());
         LessSource lessSource = new LessSource(new FileResource(sourceFile), Charset.forName(encoding));
         return (String) FieldUtils.readField(lessSource, "content", true);
     }
